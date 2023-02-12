ab379a71d5bd044701fb12fce66a2aa0387ab854
kikozai <djumvlad@yandex.ru>
2023-02-12 18:43:46 +0500

    routes

---
+++ routes/auth.route.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/Kikozai/Moon/controllers"
+)
+
+type AuthRoutes struct {
+	authController controllers.AuthController
+}
+
+func NewAuthRoutes(authController controllers.AuthController) AuthRoutes {
+	return AuthRoutes{authController}
+}
+
+func (rc *AuthRoutes) AuthRoute(rg *gin.RouterGroup) {
+
+	router := rg.Group("/auth")
+	router.POST("/register", rc.authController.SignUpUser)
+}
+
